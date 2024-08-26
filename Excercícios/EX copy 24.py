955b24a96926bb23a4313a9f0a3a1ed160506172
Lucas <lucast34t@gmail.co>
2024-08-26 10:10:30 -0300

    24 and 50

--- Excercícios/EX copy 24.py
+++ Excercícios/EX copy 24.py
@@ -1,1 +1,8 @@
 # Desenvolva um algoritmo que pergunte ao usuário o nome de uma cidade e verifique se é a capital do Brasil.
+c = 'distrito federal'
+t = str(input('Digite um estado: '))
+
+while t != c:
+    print('Não é a capital do Brasil')
+    t = str(input('Digite um estado: '))
+print('É a capital do Brasil')
---
+++ Excercícios/EX copy 51.py
@@ -0,0 +1,1 @@
+# Crie um programa que peça ao usuário para inserir números até que ele digite o número 0. Ao final, exiba a soma de todos os números inseridos (exceto o 0).
