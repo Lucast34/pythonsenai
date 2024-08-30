c90c888a662abc040ab7e7c1519076841a67a10c
Lucas <lucast34@gmail.com>
2024-08-30 17:36:22 -0300

    finalizar algumas coisas

--- Exercícios_2/jogo da velha/jogo_da_velha.py
+++ Exercícios_2/jogo da velha/jogo_da_velha.py
@@ -1,1 +1,21 @@
 # desenvolve um jogo da velha desenvolvido em python que funcione no terminal
+from pickletools import int4
+import velha_funcoes as jv
+
+jogador = 'X'
+vencedor = False
+while vencedor == False:
+    jv.desenhar_tabuleiro()
+
+    jogada = int(input('onde deseja joga? '))
+
+    jv.jogar(jogada, jogador)
+
+    jv.desenhar_tabuleiro()
+
+    jogador = jv.troca(jogador)
+    vencedor = jv.vitoria()
+
+
+jogador = jv.troca(jogador)
+print(f'Ojogador "{jogador}" venceu')
