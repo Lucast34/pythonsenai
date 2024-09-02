9c76fbd5b3bbb3c73f09bd1ee51c3b34b42b09d3
Lucas <lucast34t@gmail.co>
2024-09-02 20:51:36 -0300

    jogo da velha modifiação1

--- Exercícios_2/jogo da velha/velha_funcoes.py
+++ Exercícios_2/jogo da velha/velha_funcoes.py
@@ -25,17 +25,25 @@ def troca(jogador):
     return jog
 
 
-def vitoria():
-    if (tabuleiro[0] == tabuleiro[1] == tabuleiro[2] or
-            tabuleiro[3] == tabuleiro[4] == tabuleiro[5] or
-            tabuleiro[6] == tabuleiro[7] == tabuleiro[8]):
-        return True
-    elif (tabuleiro[0] == tabuleiro[3] == tabuleiro[6] or
-            tabuleiro[1] == tabuleiro[4] == tabuleiro[7] or
-            tabuleiro[2] == tabuleiro[5] == tabuleiro[8]):
-        return True
-    elif (tabuleiro[0] == tabuleiro[4] == tabuleiro[8] or
-          tabuleiro[2] == tabuleiro[4] == tabuleiro[6]):
-        return True
-    else:
-        return False
+def vitoria():[
+    (0, 1, 2), (3, 4, 5), (6, 7, 8),
+    (0, 3, 6), (1, 4, 7), (2, 5, 8),
+    (0, 4, 8), (2, 4, 6)
+]
+
+for a, b, c in numeros:
+    return True
+
+    # if (tabuleiro[0] == tabuleiro[1] == tabuleiro[2] or
+    #         tabuleiro[3] == tabuleiro[4] == tabuleiro[5] or
+    #         tabuleiro[6] == tabuleiro[7] == tabuleiro[8]):
+    #     return True
+    # elif (tabuleiro[0] == tabuleiro[3] == tabuleiro[6] or
+    #         tabuleiro[1] == tabuleiro[4] == tabuleiro[7] or
+    #         tabuleiro[2] == tabuleiro[5] == tabuleiro[8]):
+    #     return True
+    # elif (tabuleiro[0] == tabuleiro[4] == tabuleiro[8] or
+    #       tabuleiro[2] == tabuleiro[4] == tabuleiro[6]):
+    #     return True
+    # else:
+    #     return False
