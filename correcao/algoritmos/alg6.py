8085955b3a8e18410bc3cf4011691beb00bd2688
Lucas <lucast34@gmail.com>
2024-09-04 17:09:33 -0300

    revisao

--- correcao/algoritmos/alg6.py
+++ correcao/algoritmos/alg6.py
@@ -1,1 +1,10 @@
-# refatorar
+
+# operacoes = {
+#     '+': lambda x,y: x+y,
+#     '-': lambda x, y : x-y,
+#     '/': lambda x, y : x / y,
+#     '*': lambda x, y : x * y,
+#     }
+# if operacao in operacoes:
+#     resultado = operacoes[operacao](n1, n2)
+#     print(f'numerp {n1} {operacao) {n2}, é {resultado}
---
+++ correcao/algoritmos/alg61.py
@@ -0,0 +1,4 @@
+num= [1, 2, 3, 4, 5]
+
+for numero in num:
+    print(numero)
